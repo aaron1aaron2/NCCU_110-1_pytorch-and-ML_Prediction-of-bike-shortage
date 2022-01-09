64eb3ecbc1527de5c5b6b53d96fe2d045dd80d1e
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-01-09 21:37:27 +0800

    Update pytorch_class_pipeline.sh

--- scripts/pytorch_class_pipeline.sh
+++ scripts/pytorch_class_pipeline.sh
@@ -19,11 +19,11 @@ python data_helper_SE.py \
     --adj_threshold 0.1
 
 # train model
-python train.py --time_slot 10 --num_his 5 --num_pred 1 --batch_size 6 ^
-        --max_epoch 1 --patience 50 --learning_rate 0.001 ^
-        --traffic_file data/train_data/data.h5 ^
-        --SE_file data/train_data/SE/basic/SE.txt ^
-        --model_file ./output/basic/model.pkl ^
-        --log_file ./output/basic/log.txt ^
-        --output_folder ./output/basic ^
+python train.py --time_slot 10 --num_his 5 --num_pred 1 --batch_size 6 \
+        --max_epoch 100 --patience 100 --learning_rate 0.001 \
+        --traffic_file data/train_data/data.h5 \
+        --SE_file data/train_data/SE/basic/SE.txt \
+        --model_file ./output/basic/model.pkl \
+        --log_file ./output/basic/log.txt \
+        --output_folder ./output/basic \
         --device gpu
