07fdbe6a22adf19fdb0f9c1e20e4afc91636a96c
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-01-11 00:15:32 +0800

    Update result

--- scripts/hyperparameters_experiment/train_gpu_lr0.01.sh
+++ scripts/hyperparameters_experiment/train_gpu_lr0.01.sh
@@ -1,5 +1,5 @@
 output_folder_root="./output/hyperparameters_experiment__gpu_lr0.01"
-python train.py --time_slot 10 --num_his 5 --num_pred 1 --batch_size 40 \
+python train.py --time_slot 10 --num_his 5 --num_pred 1 --batch_size 24 \
         --max_epoch 100 --patience 100 --learning_rate 0.01 \
         --traffic_file data/train_data/data.h5 \
         --SE_file data/train_data/SE/basic/SE.txt \
@@ -7,3 +7,13 @@ python train.py --time_slot 10 --num_his 5 --num_pred 1 --batch_size 40 \
         --log_file "$output_folder_root"/log.txt \
         --output_folder $output_folder_root \
         --device gpu
+
+# set output_folder_root="./output/hyperparameters_experiment__gpu_lr0.01"
+# python train.py --time_slot 10 --num_his 5 --num_pred 1 --batch_size 24 ^
+#         --max_epoch 100 --patience 100 --learning_rate 0.01 ^
+#         --traffic_file data/train_data/data.h5 ^
+#         --SE_file data/train_data/SE/basic/SE.txt ^
+#         --model_file %output_folder_root%/model.pkl ^
+#         --log_file %output_folder_root%/log.txt ^
+#         --output_folder %output_folder_root% ^
+#         --device gpu
