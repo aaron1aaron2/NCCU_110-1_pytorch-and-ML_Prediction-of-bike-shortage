7a0158fe6e93c8dd8769a6cc6f23d85fc42e010c
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-01-11 23:45:03 +0800

    Update SE file path

--- scripts/adj_threshold_experiment/train_adj_th(0.34703)_75-percentile.sh
+++ scripts/adj_threshold_experiment/train_adj_th(0.34703)_75-percentile.sh
@@ -1,6 +1,6 @@
 output_folder_root="./output/adj_threshold_experiment__th(0.34703)_75-percentile"
 
-SEfile=".data/train_data/SE/test_adj/adj0.34703/SE.txt"
+SEfile="./data/train_data/SE/test_adj/adj0.34703/SE.txt"
 python train.py --time_slot 10 --num_his 5 --num_pred 1 --batch_size 24 \
         --max_epoch 50 --patience 100 --learning_rate 0.01 \
         --traffic_file data/train_data/data.h5 \
