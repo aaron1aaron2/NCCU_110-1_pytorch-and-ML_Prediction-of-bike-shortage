20ca05ae5aa08ac1b4484ebc3f0f9a1ed63613b5
aaron1aaron2 <46644821+aaron1aaron2@users.noreply.github.com>
2022-01-13 12:08:33 +0800

    Update train_gpu_colab2.sh

--- scripts/cpu_gpu_experiment/train_gpu_colab2.sh
+++ scripts/cpu_gpu_experiment/train_gpu_colab2.sh
@@ -1,4 +1,4 @@
-output_folder_root="./output/cpu_gpu_experiment__gpu(Tesla-K80)_epoch50"
+output_folder_root="./output/cpu_gpu_experiment__gpu(Tesla-T4)_epoch50"
 python train.py --time_slot 10 --num_his 5 --num_pred 1 --batch_size 6 \
         --max_epoch 50 --patience 100 --learning_rate 0.001 \
         --traffic_file data/train_data/data.h5 \
