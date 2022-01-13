990f8d05fb49da4845a7fa9611067a6e9d7e28ac
aaron1aaron2 <46644821+aaron1aaron2@users.noreply.github.com>
2022-01-13 12:08:42 +0800

    Update train_basic_model.sh

--- scripts/train_basic_model.sh
+++ scripts/train_basic_model.sh
@@ -1,7 +1,7 @@
 output_folder_root="./output/basic"
 SEfile="./data/train_data/SE/basic/SE.txt"
 python train.py --time_slot 10 --num_his 5 --num_pred 1 --batch_size 24 \
-        --max_epoch 50 --patience 50 --learning_rate 0.01 \
+        --max_epoch 100 --patience 100 --learning_rate 0.01 \
         --traffic_file data/train_data/data.h5 \
         --SE_file "$SEfile" \
         --model_file "$output_folder_root"/model.pkl \
