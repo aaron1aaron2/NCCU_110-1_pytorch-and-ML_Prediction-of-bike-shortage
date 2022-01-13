f8e93e5f4a10ca273e353f03291ea64a7871b319
aaron1aaron2 <46644821+aaron1aaron2@users.noreply.github.com>
2022-01-13 12:08:37 +0800

    Update train_final_model.sh

--- scripts/train_final_model.sh
+++ scripts/train_final_model.sh
@@ -1,7 +1,7 @@
 output_folder_root="./output/final"
 SEfile="./data/train_data/SE/test_adj/adj0.00124/SE.txt"
 python train.py --time_slot 10 --num_his 2 --num_pred 1 --batch_size 5 \
-        --max_epoch 50 --patience 50 --learning_rate 0.001 \
+        --max_epoch 100 --patience 100 --learning_rate 0.001 \
         --traffic_file data/train_data/data.h5 \
         --SE_file "$SEfile" \
         --model_file "$output_folder_root"/model.pkl \
